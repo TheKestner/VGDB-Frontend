7f47441057cbd8ddf24eeca9411498fc8af17ef8
Zachary Kestner <zachary.kestner@student.awesomeinc.org>
2022-04-26 00:28:12 +0000

    deleted some unused imports

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import './App.css';
 import React from "react";
-import {
-  BrowserRouter as Outlet
-} from "react-router-dom";
 import { Nav, Navbar, NavDropdown, NavLink, Container, Form, FormControl, Button} from 'react-bootstrap'
 import Header from './components/Header';
 
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
 import Game from './components/Games';
