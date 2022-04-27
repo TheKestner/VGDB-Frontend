1589c2f4207e07d5dd5558217bc58508097c9a13
Zachary Kestner <zachary.kestner@student.awesomeinc.org>
2022-04-27 15:21:44 +0000

    saving progress with getting one game to display

--- src/components/Game.js
+++ src/components/Game.js
@@ -15,7 +15,7 @@ export default function Game() {
 
     return (
       <div>
-            <h2>Game:</h2>
+        <h2> </h2>
             {items
             .filter(item => item.title === 'Hatoful Boyfriend')
             .map((item) => <Detail key={item.id} item={item} />)}
@@ -25,9 +25,13 @@ export default function Game() {
 
   const Detail = ({ item }) => {
     return (
-      <div className="title">
-      {item.title}
-      <div className="des"> Description: {item.about}</div>
+      <div className="game">
+      <h2>{item.title}</h2> 
+      <img src={item.coverart}></img>
+      {item.release_date}
+      <div className="genre"> {item.genre}</div>
+      <div className="platform"> {item.platforms}</div>
+      <div className="des"> {item.about}</div>
       </div>
     )
   }
