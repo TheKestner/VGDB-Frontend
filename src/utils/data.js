864df0f68cc6db4623b11352e245b3a86e1eb35a
Zachary Kestner <zachary.kestner@student.awesomeinc.org>
2022-04-27 23:53:58 +0000

    saving change to website api url

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export async function getData() {
-    let response = await axios.get(`https://8000-thekestner-vgdbbackend-fuft7n78fv1.ws-us42.gitpod.io/game/`);
+    let response = await axios.get(`https://8000-thekestner-vgdbbackend-fuft7n78fv1.ws-us43.gitpod.io/game/`);
     return response.data;
 }
