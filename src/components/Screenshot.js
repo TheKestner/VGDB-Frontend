e0fb145b940f252183496289f9d230f0b24462f2
Zachary Kestner <zachary.kestner@student.awesomeinc.org>
2022-05-03 21:18:26 +0000

    Screenshot component is now working correctly with game page. Only data for one game currently in the backend. May add another package to allow the image to be view as a pop out and larger

---
+++ src/components/Screenshot.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import { getData } from "../utils/data";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
+import { Container } from "react-bootstrap";
+import { useParams } from "react-router-dom";
+
+export default function Screenshot() {
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    getData().then((data) => {
+      setItems(data);
+    });
+  }, []);
+
+  let { title } = useParams();
+
+  let games = items
+  .filter(item => item.title === title)
+  .map((item) => {
+    return (
+        item.screenshot.map((item) =>
+      <SplideSlide>
+        <img className="screenshot" src={item} />
+      </SplideSlide>
+      )
+    );
+  });
+
+  return (
+      <Splide
+        default
+        options={{
+            gap         : 10,
+            rewind      : true,
+            pagination  : false,
+            isNavigation: true,
+            perPage: 3,
+        }}
+        aria-label="Popular Game Images"
+      >
+        {games}
+      </Splide>
+  );
+}
